2b60968e20c3ed05610e74d1fbfa678f7d378f54
이의진 <aghpt04@gmail.com>
2023-03-21 16:35:06 +0900

    C언어 파일 세팅

---
+++ study_C/study_C/study_C.c
@@ -0,0 +1,55 @@
+//#include <stdio.h>
+//
+//int main(void)
+//{
+//	int i, j;
+//	scanf("%d", &i);
+//	scanf("%d", &j);
+//	printf("첫번째 수 : %d\n", i);
+//	printf("두번째 수 : %d\n", j);
+//	printf("%d + %d = %d", i, j, i + j);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main(void)
+//{
+//	int i, j;
+//	scanf("%d %d", &i, &j);
+//	printf("정수 두 개를 입력하세요.(0 제외) : %d %d.\n", i, j);
+//	printf("%d + %d = %d\n", i, j, i + j);
+//	printf("%d - %d = %d\n", i, j, i - j);
+//	printf("%d * %d = %d\n", i, j, i * j);
+//	printf("%d / %d = %d\n", i, j, i / j);
+//	return 0;
+//}
+
+//#include <stdio.h>
+
+//int main(void)
+//{
+//	int i, j, k;
+//	scanf("%d %d %d", &i, &j, &k);
+//	printf("국어 점수를 입력하시오. (0제외) : %d\n", i);
+//	printf("영어 점수를 입력하시오. (0제외) : %d\n", j);
+//	printf("수학 점수를 입력하시오, (0제외) : %d\n", k);
+//	printf("세 과목의 합 : %d\n", i + j + k);
+//	printf("세 과목의 평균 : %d\n", (i + j + k) / 3);
+//	return 0;
+//}
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+	float yard, inch;
+	printf("yard 값을 입력하세요. : ");
+	scanf("%f", &yard);
+	printf("inch 값을 입략하세요. : ");
+	scanf("%f", &inch);
+
+	printf("%1f", )
+		return 0;
+}
